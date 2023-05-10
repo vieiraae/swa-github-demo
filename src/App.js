f9b09f48368acb6db5e5a5777c7163cafc0de365
Alexandre Vieira <alexandre.vieira@microsoft.com>
2023-05-10 17:02:46 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div><canvas id="canvas" width="800" height="800"></canvas></div>;
+  return <canvas id="canvas" width="800" height="800"></canvas>;
 }
 
 export default App;
