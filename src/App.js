b2afdc367e9d216c50f06daea273f65aec843c93
Alexandre Vieira <alexandre.vieira@microsoft.com>
2023-05-10 01:04:22 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function App() {
-  return '<h1>Olá GitHub!</h1><canvas id="canvas" width="800" height="800"></canvas>';
+  const value = 'GitHub!!!';
+  return <canvas id="canvas" width="800" height="800"></canvas>;
 }
 
 export default App;
